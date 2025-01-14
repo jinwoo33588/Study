ba9da561367ff07c54c81802011dce48e67e9c7d
jinwoo33588 <jinwoo3358@gmail.com>
2025-01-14 20:44:18 +0900

    1.00

--- Backjoon/backjoon_1018.java
+++ Backjoon/backjoon_1018.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class backjoon_1018 {
     public static boolean [][] arr;
     public static int min = 64;
     
